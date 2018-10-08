3984365220fbfc296fe47614ecc1721ee400e2ae
Dkazem91 <Dkazemian@gmail.com>
2018-10-08 22:54:30 +0000

    added if main check

--- 1-dictionary_of_list_of_dictionaries.py
+++ 1-dictionary_of_list_of_dictionaries.py
@@ -2,15 +2,16 @@
 #dictionary of dictionaries and API calls
 import json
 import requests
-f = open("todo_all_employees.json", "w")
-send = {}
-ids = requests.get('https://jsonplaceholder.typicode.com/users').json()
-tasks = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-for person in ids:
-    username = person['username']
-    info = [
-        {'username': username, 'task': task['title'],
-         'completed': task['completed']} for task in tasks
-        if task['userId'] == person['id']]
-    send[person['id']] = info
-json.dump(send, f)
+if __name__ == "__main__":
+    f = open("todo_all_employees.json", "w")
+    send = {}
+    ids = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    for person in ids:
+        username = person['username']
+        info = [
+            {'username': username, 'task': task['title'],
+             'completed': task['completed']} for task in tasks
+            if task['userId'] == person['id']]
+        send[person['id']] = info
+    json.dump(send, f)
